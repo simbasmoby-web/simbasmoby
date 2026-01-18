0f1048326a5f24529b2f41c7c359a1529f9bbfa2
simbasmoby-web <simbasmoby@gmail.com>
2026-01-18 12:26:35 +0100

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 const CONFIG = {
   pageTitle: "Авторськая практика від психолога",
   mainTitle: "Втомились виживати?",
-  highlight1: "15 хвилин — і ви відчуєте, що таке просто дихати. Без зусиль. Без боротьби. Просто відпустити те, що тисне.",
+  highlight1: "15 хвилин — і ви відчуєте, що таке просто дихати. <br>Без зусиль. Без боротьби. <br>Просто відпустити те, що тисне.",
   subtitle: "Авторська практика від практикуючого психолога гіпнотерапевта",
   newPrice: "149 грн",
   oldPrice: "2198 грн",
@@ -11,6 +11,7 @@ const CONFIG = {
   // background может быть относительным путём (content-images/background.jpg) или внешним URL
   background: "https://i.postimg.cc/j5KSBBXK/Gemini-Generated-Image-4rytui4rytui4ryt.png"
 };
+
 
 
 
