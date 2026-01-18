4bd01f49e31d1c800f0b7b39338cf13330a314fc
simbasmoby-web <simbasmoby@gmail.com>
2026-01-18 18:38:55 +0100

    Update config.js

--- config.js
+++ config.js
@@ -3,14 +3,15 @@ const CONFIG = {
   pageTitle: "Авторськая практика від психолога",
   mainTitle: "Втомились виживати?",
   highlight1: "Техніка, яка за 15 хвилин допоможе тобі скинути напругу, відновити енергію, послабити чи зовсім позбутись стресу та повернути внутрішню опору.
-  subtitle: "Авторська практика від практикуючого психолога гіпнотерапевта",
+  subtitle: "Від практикуючого психолога гіпнотерапевта",
   newPrice: "149 грн",
   oldPrice: "2198 грн",
-  highlight2: "Щоб позбутись стресу і напруги",
+  highlight2: "Це інвестиція в себе. В своє здоров'я. В свій спокій.",
   paymentLink: "https://psychologist-rumiantsevas-bot.tg.pulse.is",
   // background может быть относительным путём (content-images/background.jpg) или внешним URL
   background: "https://i.postimg.cc/j5KSBBXK/Gemini-Generated-Image-4rytui4rytui4ryt.png"
 };
+
 
 
 
